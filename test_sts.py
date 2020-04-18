1f13436315ab2fb610a931264d8304e686598455
TRYTOBE8TME <30604621+TRYTOBE8TME@users.noreply.github.com>
2020-04-18 13:13:28 +0530

    Add files via upload
    Two main files added

---
+++ test_sts.py
@@ -0,0 +1,175 @@
+import boto3
+import botocore.session
+from botocore.exceptions import ClientError
+from botocore.exceptions import ParamValidationError
+from nose.tools import eq_ as eq
+from nose.plugins.attrib import attr
+from nose.plugins.skip import SkipTest
+import isodate
+import email.utils
+import datetime
+import threading
+import re
+import pytz
+from collections import OrderedDict
+import requests
+import json
+import base64
+import hmac
+import hashlib
+import xml.etree.ElementTree as ET
+import time
+import operator
+import nose
+import os
+import string
+import random
+import socket
+import ssl
+import logging
+from collections import namedtuple
+
+from email.header import decode_header
+
+from . import(
+    get_iam_client,
+    get_sts_client,
+    get_s3_client
+    )
+
+def create_role(iam_client,path,rolename,policy_document,description,sessionduration,permissionboundary):
+    role_response=""
+    role_err=None
+    try:
+    	role_response = iam_client.create_role(Path=path,RoleName=rolename,AssumeRolePolicyDocument=policy_document,)
+    except ClientError as e:
+    	role_err = e.response['Code']
+    return (role_err,role_response)
+
+def put_role_policy(iam_client,rolename,policyname,role_policy):
+    role_response=""
+    role_err=None
+    try:
+        role_response = iam_client.put_role_policy(RoleName=rolename,PolicyName=policyname,PolicyDocument=role_policy)
+    except ClientError as e:
+    	role_err = e.response['Code']
+    return (role_err,role_response)
+
+def put_user_policy(iam_client,username,policyname,policy_document):
+    role_response=""
+    role_err=None
+    try:
+        role_response = iam_client.put_user_policy(UserName=username,PolicyName=policyname,PolicyDocument=policy_document)
+    except ClientError as e:
+        role_err = e.response['Code']
+    return (role_err,role_response)
+
+def test_get_session_token():
+    response=""
+    resp=""
+    resp_err=None
+    s3bucket=""
+    iam_client=get_iam_client()
+    sts_client=get_sts_client()
+    user_policy = "{\"Version\":\"2012-10-17\",\"Statement\":[{\"Effect\":\"Deny\",\"Action\":\"s3:*\",\"Resource\":[\"*\"],\"Condition\":{\"BoolIfExists\":{\"sts:authentication\":\"false\"}}},{\"Effect\":\"Allow\",\"Action\":\"sts:GetSessionToken\",\"Resource\":\"*\",\"Condition\":{\"BoolIfExists\":{\"sts:authentication\":\"false\"}}}]}"
+    (resp_err,resp)=put_user_policy(iam_client,'56789abcdef0123456789abcdef0123456789abcdef0123456789abcdef01234','Policy1',user_policy)
+    eq(resp['ResponseMetadata']['HTTPStatusCode'],200)
+    response=sts_client.get_session_token(DurationSeconds=43200)
+    eq(response['ResponseMetadata']['HTTPStatusCode'],200)
+    s3_client=boto3.client('s3',
+                aws_access_key_id = response['Credentials']['AccessKeyId'],
+		aws_secret_access_key = response['Credentials']['SecretAccessKey'],
+                aws_session_token = response['Credentials']['SessionToken'],
+		endpoint_url='http://s3.us-east.localhost:8000',
+		region_name='',
+		)
+    bucket_name = 'my-bucket'
+    s3bucket = s3_client.create_bucket(Bucket=bucket_name)
+    eq(s3bucket['ResponseMetadata']['HTTPStatusCode'],200)
+
+def test_get_session_token_permanent_creds_denied():
+    response=""
+    resp=""
+    resp_err=None
+    s3bucket_error=None
+    iam_client=get_iam_client()
+    sts_client=get_sts_client()
+    user_policy = "{\"Version\":\"2012-10-17\",\"Statement\":[{\"Effect\":\"Deny\",\"Action\":\"s3:*\",\"Resource\":[\"*\"],\"Condition\":{\"BoolIfExists\":{\"sts:authentication\":\"false\"}}},{\"Effect\":\"Allow\",\"Action\":\"sts:GetSessionToken\",\"Resource\":\"*\",\"Condition\":{\"BoolIfExists\":{\"sts:authentication\":\"false\"}}}]}"
+    (resp_err,resp)=put_user_policy(iam_client,'56789abcdef0123456789abcdef0123456789abcdef0123456789abcdef01234','Policy1',user_policy)
+    eq(resp['ResponseMetadata']['HTTPStatusCode'],200)
+    response=sts_client.get_session_token(DurationSeconds=43200)
+    eq(response['ResponseMetadata']['HTTPStatusCode'],200)
+    s3_client=get_s3_client()
+    bucket_name = 'my-bucket'
+    try:
+        s3bucket = s3_client.create_bucket(Bucket=bucket_name)
+    except ClientError as e:
+        s3bucket_error = e.response.get("Error", {}).get("Code")
+    eq(s3bucket_error,'AccessDenied')
+
+def test_assume_role_allow():
+    role_response=""
+    role_error=None
+    response=""
+    role_err=None
+    resp=""
+    string_data='Hello everyone out there'
+    iam_client=get_iam_client()    
+    sts_client=get_sts_client()
+    policy_document = "{\"Version\":\"2012-10-17\",\"Statement\":[{\"Effect\":\"Allow\",\"Principal\":{\"AWS\":[\"arn:aws:iam:::user/56789abcdef0123456789abcdef0123456789abcdef0123456789abcdef01234\"]},\"Action\":[\"sts:AssumeRole\"]}]}"
+    (role_error,role_response)=create_role(iam_client,'/','S192',policy_document,None,None,None)
+    eq(role_response['Role']['Arn'],'arn:aws:iam:::role/S192')
+    role_policy = "{\"Version\":\"2012-10-17\",\"Statement\":{\"Effect\":\"Allow\",\"Action\":\"s3:*\",\"Resource\":\"arn:aws:s3:::*\"}}"
+    (role_err,response)=put_role_policy(iam_client,'S192','Policy1',role_policy)
+    eq(response['ResponseMetadata']['HTTPStatusCode'],200)
+    resp=sts_client.assume_role(RoleArn=role_response['Role']['Arn'],RoleSessionName='Bob')
+    eq(resp['ResponseMetadata']['HTTPStatusCode'],200)
+    s3_client = boto3.client('s3',
+		aws_access_key_id = resp['Credentials']['AccessKeyId'],
+		aws_secret_access_key = resp['Credentials']['SecretAccessKey'],
+		aws_session_token = resp['Credentials']['SessionToken'],
+		endpoint_url='http://s3.us-east.localhost:8000',
+		region_name='',
+		)
+    bucket_name = 'my-bucket'
+    s3bucket = s3_client.create_bucket(Bucket=bucket_name)
+    eq(s3bucket['ResponseMetadata']['HTTPStatusCode'],200)
+    s3_client.put_object(Body=string_data,Bucket=bucket_name,Key='obj1')
+    obje = s3_client.get_object(Bucket='my-bucket',Key='obj1')
+    eq(obje['ResponseMetadata']['HTTPStatusCode'],200)
+    obj = s3_client.delete_object(Bucket='my-bucket',Key='obj1')
+    eq(obj['ResponseMetadata']['HTTPStatusCode'],204)
+    bkt = s3_client.delete_bucket(Bucket=bucket_name)
+    eq(bkt['ResponseMetadata']['HTTPStatusCode'],204)
+
+def test_assume_role_deny():
+    role_response=""
+    role_error=None
+    response=""
+    role_err=None
+    resp=""
+    s3bucket_error=None
+    iam_client=get_iam_client()    
+    sts_client=get_sts_client()
+    policy_document = "{\"Version\":\"2012-10-17\",\"Statement\":[{\"Effect\":\"Allow\",\"Principal\":{\"AWS\":[\"arn:aws:iam:::user/56789abcdef0123456789abcdef0123456789abcdef0123456789abcdef01234\"]},\"Action\":[\"sts:AssumeRole\"]}]}"
+    (role_error,role_response)=create_role(iam_client,'/','S193',policy_document,None,None,None)
+    eq(role_response['Role']['Arn'],'arn:aws:iam:::role/S193')
+    role_policy = "{\"Version\":\"2012-10-17\",\"Statement\":{\"Effect\":\"Deny\",\"Action\":\"s3:*\",\"Resource\":\"arn:aws:s3:::*\"}}"
+    (role_err,response)=put_role_policy(iam_client,'S193','Policy1',role_policy)
+    eq(response['ResponseMetadata']['HTTPStatusCode'],200)
+    resp=sts_client.assume_role(RoleArn=role_response['Role']['Arn'],RoleSessionName='Bob')
+    eq(resp['ResponseMetadata']['HTTPStatusCode'],200)
+    s3_client = boto3.client('s3',
+		aws_access_key_id = resp['Credentials']['AccessKeyId'],
+		aws_secret_access_key = resp['Credentials']['SecretAccessKey'],
+		aws_session_token = resp['Credentials']['SessionToken'],
+		endpoint_url='http://s3.us-east.localhost:8000',
+		region_name='',
+		)
+    bucket_name = 'my-bucket'
+    try:
+        s3bucket = s3_client.create_bucket(Bucket=bucket_name)
+    except ClientError as e:
+        s3bucket_error = e.response.get("Error", {}).get("Code")
+    eq(s3bucket_error,'AccessDenied')
+
